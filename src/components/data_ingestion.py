ab9c880251a7612d6e86ced8b6b66d8d8c71036e
HafedhBENAFIA88 <benafia.hafedh1988@gmail.com>
2023-07-23 03:16:34 +0200

    commiting the transformation component

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 
 
 @dataclass
@@ -44,7 +46,9 @@ class DataIngestion:
 
 if __name__== '__main__':
         obj= DataIngestion()
-        obj.initiate_data_ingestion()
+        train_path,test_path=obj.initiate_data_ingestion()
+        datatransformation=DataTransformation()
+        datatransformation.initiate_data_transformation(train_path,test_path)
 
     
 
